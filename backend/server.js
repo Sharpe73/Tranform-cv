5207094f7e10c0598e3e4a6296464da28cb3de9a
Hector Sharpe <sharpeuc@gmail.com>
2025-04-15 18:56:02 -0400

    Ajuste server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const { procesarCV } = require("./utils/procesarCV");
 const db = require("./database");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
