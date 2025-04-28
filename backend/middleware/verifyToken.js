0bf777a8d23b1f0f313a107c9f480b69469896e8
Hector Sharpe <sharpeuc@gmail.com>
2025-04-28 13:25:28 -0400

    nuevo cambio

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -12,19 +12,17 @@ async function verifyToken(req, res, next) {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // contiene id, nombre, apellido, rol
 
-    
     const userResult = await db.query("SELECT id FROM usuarios WHERE id = $1", [decoded.id]);
 
     if (userResult.rows.length === 0) {
-      console.error("❌ Usuario eliminado. Cerrando sesión.");
-      return res.status(401).json({ message: "usuario eliminado" });
+      return res.status(401).json({ message: "Usuario eliminado, sesión terminada." });
     }
 
-    req.user = decoded; // contiene id, nombre, apellido, rol
     next();
   } catch (error) {
-    console.error("❌ Token inválido o expirado:", error.message);
+    console.error("❌ Token inválido:", error.message);
     res.status(401).json({ message: "Token inválido o expirado" });
   }
 }
