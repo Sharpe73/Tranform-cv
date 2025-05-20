2ec3c5a30552898c118857a5c65e4c10e8f627cc
Hector Sharpe <sharpeuc@gmail.com>
2025-05-20 14:45:02 -0400

    ok

--- frontend/src/components/Transform.js
+++ frontend/src/components/Transform.js
@@ -140,14 +140,21 @@ function Transform() {
 
   return (
     <Container maxWidth="xl" sx={{ pt: 2, pb: 6, minHeight: "100vh" }}>
-      <Box display="flex" justifyContent="space-between" alignItems="center" mb={1} mt={1} px={2}>
-        <Typography variant="h4" color="primary" fontWeight="bold">
-          🖹 Transformar Documento
-        </Typography>
+      <Box display="flex" justifyContent="flex-end" alignItems="center" mb={1} mt={1} px={2}>
         <UserMenu />
       </Box>
 
       <Paper elevation={4} sx={{ p: { xs: 3, md: 5 }, borderRadius: 4 }}>
+        <Typography
+          variant="h4"
+          color="primary"
+          gutterBottom
+          align="center"
+          fontWeight="bold"
+        >
+          🖹 Transformar Documento
+        </Typography>
+
         <UploadSection
           file={file}
           handleFileChange={handleFileChange}
